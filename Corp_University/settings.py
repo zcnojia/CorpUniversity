d911bba0422ec5331d99bb2132e3f8bcf7b1884f
zcnojia <alyzain.maknojia@gmail.com>
2023-05-15 14:22:25 +1000

    database connection, JWT login, registration, dashboarding partial

--- Corp_University/settings.py
+++ Corp_University/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'Corp_University.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 's104310567_db',
+        'USER': 's104310567',
+        'PASSWORD': '070294',
+        'HOST': 'ictstu-db1.cc.swin.edu.au',
+        'PORT': '<port>',
     }
 }
 
